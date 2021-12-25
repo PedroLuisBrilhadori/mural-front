0a56e0a083299394a9bd392050beee38fc82083a
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-25 15:35:50 -0300

    refactor: organiza melhor as pastas e arquivos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname),
     filename: '_bundle.js'
