ccdc18345e61cb1fedc72c4ac88a042ca4545cc0
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-28 20:19:45 -0300

    refactor: reorganiza pastas

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname) + '/src/',
     filename: '_bundle.js'
   }
 };
