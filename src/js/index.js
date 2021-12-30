9fd640d99cb6a7a1ec661095d7cd1b0a2d2ad69c
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-29 21:11:31 -0300

    feat: adiciona msg inicial

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,13 @@ const url = 'https://app-mural.herokuapp.com/mural';
 const card = require("./card");
 
 
+card.msgInit({
+    title: 'Menssagens Para Vocês',
+    message: 'Este site é um muralzinho, deixe sua mensagem para quem quiser! \n\n Você pode enviar sua mensagem com até 300 caracteres (mais que o tt) e com um título. \n\n A pesquisa é por nome, ou seja, você pode pesquisar seu nome na lupinha.',
+    author: 'Pedro',
+    to: 'Todos'
+});
+
 let data = fetch(url).then(response => {
     return response.json();
 }).then();
@@ -15,7 +22,7 @@ data.then(posts => {
             author: posts[i].author,
             to: posts[i].to ? posts[i].to : null
         }     
-        card.cardCreate(post, index)  
+        card.cardCreate(post, index + 1)  
     })
 
     card.cardFilter();
